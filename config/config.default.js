4ba87450c4cd39dfad17b33c5b0199a04a356840
ijse <i@ijser.cn>
2018-01-07 16:40:51 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1515303009061_5919';
+
+  // add your config here
+  config.middleware = [];
+
+  config.view = {
+    defaultViewEngine: 'nunjucks'
+  }
+
+  config.static = {
+    prefix: ''
+  }
+
+  return config;
+};

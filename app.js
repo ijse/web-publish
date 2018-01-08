cfe5aed98e0235e6dfddd0b165f21effe7a695a5
ijse <i@ijser.cn>
2018-01-08 23:08:22 +0800

    update router settings

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const setupPassport = require('./app/passport')
 module.exports = app => {
   if (app.config.env === 'local') {
     app.beforeStart(async () => {
-      app.logger.debug('app beforeStart')
       await app.model.sync({force: true});
     })
   }
--- app/router.js
+++ app/router.js
@@ -2,9 +2,10 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller, passport } = app
-  router.get('/', controller.home.index)
-  router.post('/register', controller.user.register)
+  const { router, controller:ctl, passport } = app
+
+  router.get('/', ctl.home.index)
+  router.post('/register', ctl.user.register)
   router.post('/login', passport.authenticate('local'))
-  router.get('/logout', controller.user.logout)
+  router.get('/logout', ctl.user.logout)
 }
